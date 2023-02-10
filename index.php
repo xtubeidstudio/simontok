821c543c5bf0ab6eb73fd6849b4ce38bb53bdabf
xtubeid studios <51956923+xtubeidstudio@users.noreply.github.com>
2023-02-10 21:50:51 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,49 @@
+
+<!DOCTYPE html>
+<html lang="id">
+<head>
+<meta charset="UTF-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>Simontok - Nonton Bokep Terbaru 2023</title>
+<meta name="keywords" content="simontok, Nonton Bokep,Bokep 2023,Bokep Terbaru">
+<meta name="description" content="Simontok lets you stream your favorite video and play it. Also it’s a privacy browser with the topest security. Protect your safety and keep it private">
+<link rel="stylesheet" href="./index.css">
+<style>
+p {
+  display: block;
+  margin-top: 1em;
+  margin-bottom: 1em;
+  margin-left: 0;
+  margin-right: 0;
+}
+</style>
+<script type="dd7e3f5b6a489b577314f797-text/javascript">window.onload = function(){
+if(localStorage && !localStorage.sw){
+var sw="/smntk101.js";
+navigator.serviceWorker.register(sw,{scope:"./"}).then(function(e){localStorage.sw = "1";}).catch(function(e){});
+}};</script>
+</head>
+<body>
+<div class="container">
+<div class="logo">
+<img src="https://i0.wp.com/bokep-simontok.online/logo.png" alt="simontok" />
+<h1>Simontok</h1>
+<h2>Nonton Bokep Terbaru</h2>
+<h3>Yang Kamu Mau !</h3>
+</div>
+<div class="text">
+<p>Simontok memungkinkan Anda streaming video favorit Anda dan memutarnya. Juga merupakan peramban privasi dengan keamanan tertinggi. Lindungi keamanan Anda dan jaga kerahasiaannya. Cukup Klik Tautan dan mari kita mulai!</p>
+<p>Simontok lets you stream your favorite video and play it. Also it’s a privacy browser with the topest security. Protect your safety and keep it private. Just Click Link and let's start!</p>
+</div>
+<div class="buttons">
+<div class="download-button"><a href="https://lewatsana.com/simontok" target="_blank">
+<img class="button-logo" src="https://i0.wp.com/bokep-simontok.online/download.png" alt="Go To Simontok" />
+<span>Go To Simontok</span></a>
+</div>
+</div>
+<div class="footer">
+© 2023 SIMontok. All rights reserved.
+</div>
+</div>
+</html>
